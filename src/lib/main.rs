a8a58e62e2ec4faaf6c04f1aa1fecb77cc18b3bb
Vallabh Kansagara <vrkansagara@gmail.com>
2022-12-28 15:15:27 +0530

    adding basic point and direction

--- src/lib/main.rs
+++ src/lib/main.rs
@@ -1,5 +1,20 @@
 #![allow(dead_code, unused)]
 
+// Struct
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
 fn main() {
+    let origin: Point = Point { x: 0, y: 0 };
+    let up = Direction::Up;
     println!("Silent is golden !");
 }
